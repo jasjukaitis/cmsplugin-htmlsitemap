67a91742f2ff02b9bed9e81ace751e207ef18aad
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2012-12-12 15:54:18 +0100

    Version updated to 0.1.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmsplugin-htmlsitemap',
-    version='0.1.1',
+    version='0.1.2',
     description='HTML sitemap plugin for Django CMS',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Raphael Jasjukaitis',
